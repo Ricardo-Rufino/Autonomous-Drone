c880ed9cb7b8e9d9c538b4ede89415f3f6051d0a
Ricky <ricardo.rufino.m@gmail.com>
2020-07-12 16:05:40 -0600

    Made small, formatting changes.

--- Knob/Knob.ino
+++ Knob/Knob.ino
@@ -5,25 +5,25 @@
 
 #include <Servo.h>
 
-Servo myservo;                         // create servo object to control a servo
+Servo myservo;                          // create servo object to control a servo
 Servo oneOne, oneTwo, twoOne, twoTwo;
 
-int potPin = A0;                       // analog pin used to connect the potentiometer
-int val;                               // variables to read the value from the analog pin
+int potPin = A0;                        // analog pin used to connect the potentiometer
+int val;                                // variables to read the value from the analog pin
 
 void setup() {
   Serial.begin(9600);
-  oneOne.attach(9);  // attaches the servo on pin 9 to the servo object
+  oneOne.attach(9);                     // attaches the servo on pin 9 to the servo object
   oneTwo.attach(6);
   twoOne.attach(5);
   twoTwo.attach(3);
 }
 
 void loop() {
-  val = analogRead(potPin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  val = analogRead(potPin);             // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);      // scale it to use it with the servo (value between 0 and 180)
   
-  oneOne.write(val);                  // sets the servo positions according to the scaled value
+  oneOne.write(val);                    // sets the servo positions according to the scaled value
   oneTwo.write(val);
   twoOne.write(val);
   twoTwo.write(val);
@@ -32,4 +32,3 @@ void loop() {
   Serial.println(val);
   
 }
-
